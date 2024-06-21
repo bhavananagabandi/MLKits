1394649465c484ff1d292d9db490c912e213f1b5
Bhavana Nagabandi <bhanu@Bhavanas-MBP.attlocal.net>
2024-06-20 17:30:16 -0700

    empty line

--- plinko/score.js
+++ plinko/score.js
@@ -2,6 +2,7 @@ const outputs = [];
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   outputs.push([dropPosition, bounciness, size, bucketLabel]);
+  
   console.log(outputs);
 }
 
