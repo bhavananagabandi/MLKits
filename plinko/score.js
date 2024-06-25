ab9f1aee758039ddaec0313201e72b2dd9229cb4
Bhavana Nagabandi <bhanu@Bhavanas-MBP.attlocal.net>
2024-06-24 19:00:12 -0700

    knn for different k values

--- plinko/score.js
+++ plinko/score.js
@@ -2,11 +2,43 @@ const outputs = [];
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   outputs.push([dropPosition, bounciness, size, bucketLabel]);
-  
-  console.log(outputs);
 }
 
 function runAnalysis() {
-  // Write code here to analyze stuff
+  const testSetSize = 100;
+  const [testSet, trainingSet] = splitDataSet(outputs, testSetSize);
+
+  _.range(1,20).forEach(k => {
+    const accuracy = _.chain(testSet)
+  .filter(testPoint => knn(trainingSet, testPoint[0], k) === testPoint[3])
+  .size()
+  .divide(testSetSize)
+  .value();
+
+  console.log('For k of ', k, 'accuracy is ', accuracy);
+  });
+}
+const knn = (data, point, k) => {
+  return _.chain(data)
+  .map(row => [distance(row[0], point), row[3]])
+  .sortBy(row => row[0])
+  .slice(0, k)
+  .countBy(row => row[1])
+  .toPairs()
+  .sortBy(row => row[1])
+  .last()
+  .first()
+  .parseInt()
+  .value();
+}
+const distance = (pointA, pointB) => Math.abs(pointA-pointB);
+
+const splitDataSet = (data, testCount) => {
+  const shuffled = _.shuffle(data)
+
+  const testSet = _.slice(shuffled, 0, testCount)
+  const trainingSet = _.slice(shuffled, testCount)
+
+  return [testSet, trainingSet]
 }
 
